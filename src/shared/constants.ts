16f69e690f4fcf5213d9bbcaf6d54d196fb6b41a
Andrey <zertsalov96@yandex.ru>
2024-12-14 17:55:03 +0000

    Zerion Client first iteration

---
+++ src/shared/constants.ts
@@ -0,0 +1,10 @@
+import { ChainInfo } from "./types";
+
+export const ALL_CHAINS = "all";
+export const ZERO_CHAIN_CONFIG: ChainInfo = {
+  iconUrl: "https://chain-icons.s3.amazonaws.com/chainlist/543210",
+  id: "zero",
+  name: "ZERϴ",
+  testnet: false,
+};
+export const MENU_BAR_ADDRESS_KEY = "menu-bar-address";
